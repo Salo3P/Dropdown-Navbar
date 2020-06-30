be3645bb1f15dc7f81a31c26c6daa8a7fdf3beec
Salo3P <67271670+Salo3P@users.noreply.github.com>
2020-06-30 14:53:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const MenuSlider = () => {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu-links')
+
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('active');
+
+        burger.classList.toggle('toggle');
+    });
+
+
+}
+
+MenuSlider();
